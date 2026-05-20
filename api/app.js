fbcd74fec33ac871b043d87ebb71962dc5551dd8
azmat855 <afridiazmat63@gmail.com>
2026-05-21 00:32:40 +0500

    changes

--- api/app.js
+++ api/app.js
@@ -68,13 +68,11 @@ app.get(
 app.use(errorHandler);
 
 // SERVER
-const PORT = process.env.PORT,
+const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV !== "production") {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = app;
 
